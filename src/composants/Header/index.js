c4b58ae2c1c48f45bc643eb5c591dbecbff76807
JordanMarcos <jordan.marcos.oclock@gmail.com>
2022-04-21 18:34:21 +0200

    header component done but the menu is not working

--- src/composants/Header/index.js
+++ src/composants/Header/index.js
@@ -5,9 +5,9 @@ import { MdOutlineMenu } from 'react-icons/md';
 // == Composant
 const Header = () => (
   <div className="header-container">
-    <h1 className="header-name"> <a href="https://www.linkedin.com/in/jordan-marcos/" target="_blank"> <span className="header-name-underline">Jordan</span>  Marcos </a> </h1>
+    <h1 className="header-name"> <a href="https://www.linkedin.com/in/jordan-marcos/" target="_blank" title="Aller sur mon Linkedin"> <span className="header-name-underline" >Jordan</span>  Marcos </a> </h1>
     <div className="header-container-menu">
-      <MdOutlineMenu size="3em" color='#565656' title="menu icon"/>
+      <MdOutlineMenu size="3em" color='#565656' title="menu"/>
     </div>
     
   </div>
