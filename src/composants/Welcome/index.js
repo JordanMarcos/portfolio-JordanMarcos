ed8a74c1c95d2998c9b969e04d28abdebec44598
JordanMarcos <jordan.marcos.oclock@gmail.com>
2022-04-25 11:59:07 +0200

    welcome done but not responsive

--- src/composants/Welcome/index.js
+++ src/composants/Welcome/index.js
@@ -4,9 +4,14 @@ import './styles.scss';
 // == Composant
 const Welcome = () => (
   <div className="welcome-container">
-    <div className="welcome-image-container"></div> 
-    <div className="welcome-text"> hello</div>
-    <div className="welcome-post"> dev </div>
+    
+    <div className="welcome-container-items">
+     <div className="welcome-image-container"> <img className="pic-of-me" src="https://zupimages.net/up/22/17/gdu1.jpg" alt="Photo de profil" title="photo de profil"/> </div> 
+      <div className="welcome-text"> <p className="welcome-text-p"> Bonjour, bienvenue sur mon portfolio </p> </div>
+    </div>
+
+    <div className="welcome-post"> <p className="welcome-post-p">  Développeur web full stack </p> </div>
+    
   </div>
 );
 
